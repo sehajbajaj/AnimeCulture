e84d6ec3559aebc4ff7a7989c2bfa833ba813e5e
sehajbajaj <sehajbajaj2001@gmail.com>
2022-05-27 23:49:50 +0530

    Fixing CORS

--- client/src/redux/api/recommendations.js
+++ client/src/redux/api/recommendations.js
@@ -2,4 +2,4 @@ import axios from "axios";
 
 const url = "https://animeculture.herokuapp.com/api/v1/anime";
 export const recommendAnime = (animeId) =>
-  axios.get(`${url}/${animeId}/recommend`);
+  axios.get(`${url}/${animeId}/recommend/`);

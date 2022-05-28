ab2a38690c2f9059254d2966871d213758b45346
sehajbajaj <sehajbajaj2001@gmail.com>
2022-05-28 20:15:04 +0530

    Fix CORS errors

--- client/src/redux/api/recommendations.js
+++ client/src/redux/api/recommendations.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 /* Recommendations from the Django Framework */
 /* For deployment purposes add the link to your website instead */
-const url = "https://animeculture.herokuapp.com/api/v1/anime";
+const url = "http://127.0.0.1:8000/api/v1/anime";
 export const recommendAnime = (animeId) =>
   axios.get(`${url}/${animeId}/recommend/`);

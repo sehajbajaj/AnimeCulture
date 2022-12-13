2e0303c7e6b23e9359997ba3650d813c25ee3b64
sehajbajaj <sehajbajaj2001@gmail.com>
2022-12-13 12:13:58 +0530

    Deployment

--- client/src/redux/api/recommendations.js
+++ client/src/redux/api/recommendations.js
@@ -3,6 +3,6 @@ import axios from "axios";
 /* Recommendations from the Django Framework */
 /* For deployment purposes add the link to your website instead */
 // const url = "http://127.0.0.1:8000/api/v1/anime";
-const url = "https://animesculture.up.railway.app/";
+const url = "https://animesculture.up.railway.app/api/v1/anime";
 export const recommendAnime = (animeId) =>
   axios.get(`${url}/${animeId}/recommend/`);
